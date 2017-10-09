afe452ed0abac22bef0b40db6b332f28021dc8b5
AndreFCruz <andrecruz97@gmail.com>
2017-10-09 22:16:40 +0100

    added today's exercises

--- TPs/PL_exercises_3.pl
+++ TPs/PL_exercises_3.pl
@@ -1,4 +1,55 @@
-/* TP3 - 09/10/2017 */
+translate(0, T) :- T = ' '.
+translate(1, T) :- T = 'O'. 
+
+initialBoard([[1,0,0,0],
+              [0,1,0,0],
+              [0,0,1,0],
+              [0,0,0,1]]).
+
+printMatrix([]).
+printMatrix([Line|M]) :-
+        write('|'),
+        printLine(Line),
+        write('|'), nl,
+        printMatrix(M).
+
+printLine([]).
+printLine([Head|Tail]) :-
+        translate(Head, T),
+        write(T),
+        write(' '),
+        printLine(Tail).
+
+% Ex. LIST 3
+appendList([], L, L).
+appendList([X|L1], L2, [X|L3]) :-
+        appendList(L1, L2, L3).
+
+% Ex. LIST 4
+inverter(Lista,InvLista) :-
+        rev(Lista, [], InvLista).
+rev([], InvLista, InvLista).
+rev([X|L1], L2, Inv) :-
+        rev(L1, [X|L2], Inv).
+
+% Ex. LIST 5
+% 5.a)
+/*
+membro(M, [M|_]).
+membro(M, [_|L]) :- membro(M,L).
+*/
+
+% 5.b)
+membro(M, L) :- append(_, [M|_], L). 
+
+% 5.c)
+ultimo(L, X) :- append(_, [X], L).
+
+% 5.d)
+nth_member(0, M, [M|_]).
+nth_member(N, M, [_|L]) :-
+        N > 0, N1 is N - 1,
+        nth_member(N1, M, L).
 
 /* Ex.6 */
 delete_one(X, L1, L2) :- 
@@ -18,6 +69,11 @@ delete_all_list(LX, L1, L2) :-
 
 delete_all_list([], L, L).
 
+% 7.a)
+before(A,B,L) :-
+        append(_, [A|L1], L),
+        append(_, [B|_], L1).
+
 /* Ex.8 */
 conta(Lista, N) :-
         contagem(Lista, 0, N).
@@ -29,18 +85,29 @@ contagem(Lista, Acc, N) :-
 
 contagem([], N, N).
 
+/* Ex. 8.b) */
+/*
 conta_elem(X, Lista, N) :-
                 contagem_elem(X, Lista, 0, N).
-
 contagem_elem(X, Lista, Acc, N) :-
         Lista = [H|T],
         H == X,
         Acc1 is (Acc + 1),
         contagem_elem(X, T, Acc1, N).
-
 contagem_elem(X, Lista, Acc, N) :-
         Lista = [H|T],
         H =\= X,
         contagem_elem(X, T, Acc, N).
+contagem_elem(_, [], N, N).
+*/
 
-contagem_elem(X, [], N, N).
+% 8.b) - Meh...
+conta_elem(_,[],0).
+conta_elem(X, [X|L], N) :-
+        N > 0, N1 is N - 1,
+        conta_elem(X, L, N1).
+conta_elem(X, [_|L], N) :-
+        N > 0,
+        conta_elem(X, L, N).
+
+
