33baf722c177a7cc07f281710c770241b520517e
EdgarACarneiro <up201503784@fe.up.pt>
2017-10-12 23:13:03 +0100

    Printing a nice board

--- 1º Projeto/fabrik.pl
+++ 1º Projeto/fabrik.pl
@@ -3,36 +3,44 @@ createBoard(Board, N) :-
         createBoard(Board, N , 0).
 
 createBoard(_, N, N).
-createBoard(Board, N, Lines) :-
+createBoard([FirstRow | OtherRows], N, Lines) :-
         Lines1 is (Lines+1),
-        Board = [Head | Tail],
-        createBoardLine(Head, N),
-        createBoard(Tail, N, Lines1).
+        createBoardLine(FirstRow, N),
+        createBoard(OtherRows, N, Lines1).
         
 createBoardLine(_, 0).
-createBoardLine(Board, N) :-
-        Board = [Head | Tail],
-        Head = 0,
+createBoardLine([FirstEle | OtherEle], N) :-
+        FirstEle = 0,
         N1 is (N-1),
-        createBoardLine(Tail, N1).
+        createBoardLine(OtherEle, N1).
         
-%Board Screen - Printing
-
+%Board Printing - Character Translation
 translate(0, Symbol) :- Symbol = ' '.
-translate(1, Symbol) :- Symbol = 'O'.
-translate(2, Symbol) :- Symbol = 'X'.
-transalte(3, Symbol) :- Symbol = 'R'.
+translate(1, Symbol) :- Symbol = 'O'. %Dark Pieces
+translate(2, Symbol) :- Symbol = 'X'. %White Pieces
+transalte(3, Symbol) :- Symbol = 'R'. %Red Workers
 
-printBoard([]).
-printBoard([Line|Board]) :-
+%Board Printing - arguments: Board and Board size
+printBoard([], N):-
+        printRowDivider(N).
+
+printBoard([Line|Board], N) :-
+        printRowDivider(N), nl,
         write('|'),
-        printLine(Line),
-        write('|'), nl,
-        printBoard(Board).
+        printLine(Line), nl,
+        printBoard(Board, N).
 
 printLine([]).
 printLine([Head|Tail]) :-
         translate(Head, Symbol),
-        write(Symbol),
         write(' '),
+        write(Symbol),
+        write(' |'),
         printLine(Tail).
+
+printRowDivider(0).
+printRowDivider(N) :-
+        write(' '),
+        put_code(9472), put_code(9472), put_code(9472),
+        N1 is (N-1),
+        printRowDivider(N1).
