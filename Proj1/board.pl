fe2d92588fadbbe649ce6e3eab5ee27de152d603
AndreFCruz <andrecruz97@gmail.com>
2017-10-30 20:08:07 +0000

    minor changes

--- Proj1/board.pl
+++ Proj1/board.pl
@@ -1,9 +1,3 @@
-% Dictionary for Board Elements
-translate(none, Code) :- Code = 32.
-translate(black, Code) :- Code = 79. %Dark Pieces
-translate(white, Code) :- Code = 88. %White Pieces
-translate(worker, Code) :- Code = 9608. %Red Workers
-
 % Generate a board predicate with N x N empty spaces
 createBoard(Board, N) :-
         createBoard(Board, N , 0).
@@ -20,84 +14,24 @@ createBoardLine([FirstEle | OtherEle], N) :-
         N1 is (N-1),
         createBoardLine(OtherEle, N1).
 
-% Board Printing - arguments: Board and Board size
-printFabrik(Board, N):-
-        nl, 
-        write('  '), printHorizontalLabel(N, N),
-        printBoard(Board, N, 1).
 
-printBoard([], N, _):-
-        printRowDivider(N), nl.
+% Access the element in the [Row,Col] position of the given board
+getElementAux(0, [Elem | _], Element):-
+        Element = Elem, !. % added a cut
 
-printBoard([Line | Board], N, CurrentL) :-
-        printRowDivider(N),
-        printDesignRow(N),
-        printVerticalLabel(CurrentL),
-        put_code(9474),
-        printLine(Line),
-        printDesignRow(N),
-        NewL is (CurrentL+1),
-        printBoard(Board, N, NewL).
+getElementAux(Col, [_ | RemainderOfLine], Element) :-
+        Col1 is (Col-1),
+        getElementAux(Col1, RemainderOfLine, Element).
 
-printLine([]) :- nl.
-printLine([Head | Tail]) :-
-        translate(Head, Code),
-        write('   '),
-        put_code(Code),
-        write('   '), put_code(9474),
-        printLine(Tail).
+getElement(0, Col, [Line | _], Element):-
+        getElementAux(Col, Line, Element).
 
-%  AESTHETICS
+getElement(Row, Col, [_ | RemainderRows], Element):-
+        Row1 is (Row-1),
+        getElement(Row1, Col, RemainderRows, Element).
 
-printRowDivider(N):-
-        write('  '),
-        put_code(9532),
-        printRowDividerRec(N).
 
-printRowDividerRec(0) :- nl.
-printRowDividerRec(N) :-
-        put_code(9472), put_code(9472), put_code(9472), put_code(9472),
-        put_code(9472), put_code(9472), put_code(9472), put_code(9532),
-        N1 is (N-1),
-        printRowDividerRec(N1).
+% Set piece on board -- not yet validating move
+%% ...
+%% ...
 
-printDesignRow(N):-
-        write('  '),
-        put_code(9474),
-        printDesignRowRec(N).
-
-printDesignRowRec(0) :- nl.
-printDesignRowRec(N) :-
-        write('       '), put_code(9474),
-        N1 is (N-1),
-        printDesignRowRec(N1).
-
-%Dictionary for Labels
-getLabel( 0, L):- L = 'A'.
-getLabel( 1, L):- L = 'B'.
-getLabel( 2, L):- L = 'C'.
-getLabel( 3, L):- L = 'D'.
-getLabel( 4, L):- L = 'E'.
-getLabel( 5, L):- L = 'F'.
-getLabel( 6, L):- L = 'G'.
-getLabel( 7, L):- L = 'H'.
-getLabel( 8, L):- L = 'I'.
-getLabel( 9, L):- L = 'J'.
-getLabel(10, L):- L = 'K'.
-getLabel(11, L):- L = 'L'.
-
-printHorizontalLabel(0, _):- nl.
-printHorizontalLabel(N, Total):-
-        Pos is (Total-N),
-        getLabel(Pos, L),
-        write('    '), write(L), write('   '),
-        N1 is (N-1),
-        printHorizontalLabel(N1, Total).        
-
-printVerticalLabel(CurrentL):-
-        CurrentL < 10,
-        write(CurrentL),
-        write(' ').
-
-printVerticalLabel(CurrentL):-
-        write(CurrentL).
--- Proj1/fabrik.pl
+++ Proj1/fabrik.pl
@@ -1,27 +1,18 @@
 :- include('board.pl').
+:- include('display.pl').
 :- include('utils.pl').
 :- include('menus.pl').
 
 % Utils
 boardSize(9).
 boardSize(11).
+
+initGame :-
+        boardSize(N),
+        initGame(N).
 initGame(N) :-
         boardSize(N),
         createBoard(B, N),
         printFabrik(B, N).
         
 
-%Access the element in the [Row,Col] position of the given board
-accessElement(0, [Elem | _], Element):-
-        Element = Elem.
-
-accessElement(Col, [_ | Board], Element) :-
-        Col1 is (Col-1),
-        accessElement(Col1, Board, Element).
-
-accessElement(0, Col, [Line | _], Element):-
-        accessElement(Col, Line, Element).
-
-accessElement(Row, Col, [_ | Board], Element):-
-        Row1 is (Row-1),
-        accessElement(Row1, Col, Board, Element).
