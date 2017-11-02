9cf1efa921b4a189c20943b08d03e17029a19d1d
AndreFCruz <andrecruz97@gmail.com>
2017-11-02 18:47:02 +0000

    tiny changes

--- Proj1/board.pl
+++ Proj1/board.pl
@@ -21,17 +21,17 @@ createBoardLine([FirstEle | OtherEle], N) :-
 
 
 % Access the element in the [Row,Col] position of the given board
-getElementAux(0, [Elem | _], Element):-
+getElementAux(0, [Elem | _], Element) :-
         Element = Elem, !. % added a cut
 
 getElementAux(Col, [_ | RemainderOfLine], Element) :-
         Col1 is (Col - 1),
         getElementAux(Col1, RemainderOfLine, Element).
 
-getElement(0, Col, [Line | _], Element):-
+getElement(0, Col, [Line | _], Element) :-
         !, getElementAux(Col, Line, Element).
 
-getElement(Row, Col, [_ | RemainderRows], Element):-
+getElement(Row, Col, [_ | RemainderRows], Element) :-
         Row1 is (Row-1),
         getElement(Row1, Col, RemainderRows, Element).
 
@@ -64,24 +64,24 @@ setPiece(Piece, Row, Col, Board, NewBoard) :-
 
 
 % Gets the Intersections between the two workers, into an array of positions
-getPossiblePositions(Board, _Positions):- % % TODO - also, assess if this should exist
+getPossiblePositions(Board, _Positions) :- % % TODO - also, assess if this should exist
         findWorker(Board, 0, 0, _WorkerRow, _WorkerCol).
 
-findBothWorkers(Board, Positions):-
+findBothWorkers(Board, Positions) :-
         findWorker(Board, 0, 0, WorkerRow1, WorkerCol1),
         nextPos(Board, WorkerRow1, WorkerCol1, NextRow, NextCol),
         findWorker(Board, NextRow, NextCol, WorkerRow2, WorkerCol2),
         Positions = [[WorkerRow1, WorkerCol1], [WorkerRow2, WorkerCol2]].
 
 % Finds the next Worker starting from the given Row and Column.
-findWorker(Board, Row, Col, WorkerRow, WorkerCol):-
+findWorker(Board, Row, Col, WorkerRow, WorkerCol) :-
         getElement(Row, Col, Board, Element),
         Element == worker, !, % TODO Assess cut usefulness.
         WorkerRow = Row,
         WorkerCol = Col.
 
 % Updates the Current Position and retries
-findWorker(Board, Row, Col, WorkerRow, WorkerCol):-
+findWorker(Board, Row, Col, WorkerRow, WorkerCol) :-
         nextPos(Board, Row, Col, NextRow, NextCol),
         findWorker(Board, NextRow, NextCol, WorkerRow, WorkerCol).
 
@@ -114,43 +114,44 @@ checkRowWin(Side, [FirstEl | RestOfRow], _Count) :-
 
 
 % Check Vertical Win for side 'Side'
-checkVerticalWin(Side, Board):-
+checkVerticalWin(Side, Board) :-
         transpose(Board, TransposedBoard),
         checkHorizontalWin(Side, TransposedBoard).
 
 
 % Check Diagonal Win for side 'Side'
-checkDiagonalWin(Side, Board):-
+checkDiagonalWin(Side, Board) :-
         length(Board, Size),
         checkDiffDiagonals(Side, Board, Size).
 
 %Check diagonals from [0, 0] to [0, BoardSize]
-checkDiffDiagonals(Side, Board, BoardSize):-
+checkDiffDiagonals(Side, Board, BoardSize) :-
         checkDiagonalWinAux(Side, Board, 0, BoardSize).
 %Check diagonals from [0, 0] to [BoardSize, 0]
-checkDiffDiagonals(Side, Board, BoardSize):-
+checkDiffDiagonals(Side, Board, BoardSize) :-
         transpose(Board, TransposedBoard),
         checkDiagonalWinAux(Side, TransposedBoard, 0, BoardSize).
 
 %Checks the different diagonal lines of the given board starting at [0, 0] ending at [0, BoardSize].
-checkDiagonalWinAux(Side, Board, Col, BoardSize):-
+checkDiagonalWinAux(Side, Board, Col, BoardSize) :-
         NumPositions is (BoardSize - Col),
-        NumPositions >= 5,       %Number of cells there must be to happen N in a row
+        winningStreakN(WinN),
+        NumPositions >= WinN,       %Number of cells there must be to happen N in a row
         checkDiagLineWin(Side, Board, Col, NumPositions).
-checkDiagonalWinAux(Side, Board, Col, BoardSize):-
+checkDiagonalWinAux(Side, Board, Col, BoardSize) :-
         NewCol is (Col + 1),
         NewCol < BoardSize,
         checkDiagonalWinAux(Side, Board, NewCol, BoardSize).
 
 %Check If there are N in a row, in the given diagonal line.
 %Starts at Position [0, Col], and Line has NumPositions to evaluate.
-checkDiagLineWin(Side, Board, Col, NumPositions):-
+checkDiagLineWin(Side, Board, Col, NumPositions) :-
         checkDiagLineWinAux(Side, Board, 0, Col, NumPositions, 0, _Count).
 
-checkDiagLineWinAux(Side, Board, Row, Col, _NumPos, CurrCount, UpdatedCount):-
+checkDiagLineWinAux(Side, Board, Row, Col, _NumPos, CurrCount, UpdatedCount) :-
         checkDiagonalElem(Side, Board, Row, Col, CurrCount, UpdatedCount),
         winningStreakN(UpdatedCount), !.
-checkDiagLineWinAux(Side, Board, Row, Col, NumPos, CurrCount, UpdatedCount):-
+checkDiagLineWinAux(Side, Board, Row, Col, NumPos, CurrCount, UpdatedCount) :-
         checkDiagonalElem(Side, Board, Row, Col, CurrCount, UpdatedCount),
         NumPos > 0,
         NewRow is (Row + 1),
@@ -159,9 +160,9 @@ checkDiagLineWinAux(Side, Board, Row, Col, NumPos, CurrCount, UpdatedCount):-
         checkDiagLineWinAux(Side, Board, NewRow, NewCol, NewNumPos, UpdatedCount, _NewCount).
 
 %Check the Element in the given position and update Count.
-checkDiagonalElem(Side, Board, Row, Col, Count, NewCount):-
+checkDiagonalElem(Side, Board, Row, Col, Count, NewCount) :-
         getElement(Row, Col, Board, Element),
         Element = Side, !, 
         NewCount is (Count + 1).
-checkDiagonalElem(_Side, _Board, _Row, _Col, _Count, NewCount):-
+checkDiagonalElem(_Side, _Board, _Row, _Col, _Count, NewCount) :-
         NewCount is 0.
