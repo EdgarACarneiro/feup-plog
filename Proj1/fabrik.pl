c9ce0a38984f5fdb98049ed3084a9c862c63a6dc
EdgarACarneiro <up201503784@fe.up.pt>
2017-11-12 19:15:06 +0000

    Full Board test and call

--- Proj1/fabrik.pl
+++ Proj1/fabrik.pl
@@ -26,6 +26,7 @@ initGame(_, _):-
 
 %game Loop for Player 1
 gameLoop(Player1Function, Player2Function, black, Board) :-
+	boardIsNotFull(Board),
 	call(Player1Function, black, Board, NewBoard), printBoard(NewBoard),
 	decideNextStep(Player1Function, Player2Function, black, NewBoard), !.
 %Player 1 could not place a piece, so he lost
@@ -33,6 +34,7 @@ gameLoop(_Player1Function, _Player2Function, black, _Board) :- victory(white), !
 
 %game Loop for Player 2
 gameLoop(Player1Function, Player2Function, white, Board) :-
+	boardIsNotFull(Board),
 	call(Player2Function, white, Board, NewBoard), printBoard(NewBoard),
 	decideNextStep(Player1Function, Player2Function, white, NewBoard), !.
 %Player 2 could not place a piece, so he lost
