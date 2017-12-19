2c9fc1fdc11f25651581456a23ab35dbb1306323
AndreFCruz <andrecruz97@gmail.com>
2017-12-19 11:36:30 +0000

    testing performance

--- Proj2/skyscraper.pl
+++ Proj2/skyscraper.pl
@@ -1,7 +1,8 @@
 :- include('solver.pl').
 
 % fd_statistics(backtracks, B).
-% testRestrictions(R), solveBoard(R, B), printBoard(B).
+% testRestrictions4(R), solveBoard(R, B), printBoard(B).
+
 
 skyscraper :-
   write('Skyscraper!'), nl,
@@ -20,9 +21,35 @@ printRow([El | Row]) :-
 
 
 /**  Test Boards  **/
+%% 6 by 6 board -- given example
 testRestrictions([
   [5, 0, 0, 2, 2, 0],
   [0, 2, 3, 4, 0, 0],
   [0, 3, 4, 0, 0, 4],
   [0, 0, 4, 3, 2, 0]
 ]).
+
+%% 5 by 5 board
+testRestrictions2([
+  [4, 0, 1, 2, 3],
+  [0, 2, 0, 4, 0],
+  [0, 0, 4, 0, 0],
+  [0, 0, 0, 0, 2]
+]).
+
+%% 4 by 4 board
+testRestrictions3([
+  [4, 0, 0, 2],
+  [0, 3, 0, 0],
+  [0, 0, 4, 0],
+  [0, 0, 0, 3]
+]).
+
+%% 7 by 7 board
+testRestrictions4([
+  [5, 0, 0, 2, 2, 0, 0],
+  [0, 2, 3, 4, 0, 0, 0],
+  [0, 3, 4, 0, 0, 4, 0],
+  [0, 0, 4, 3, 2, 0, 0]
+]).
+
